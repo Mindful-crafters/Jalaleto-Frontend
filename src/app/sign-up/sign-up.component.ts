1a611875c4232ad27b2b2f06413adc6bb3948bd2
sadegh zareian <m_zareian@comp.iust.ac.ir>
2023-11-03 15:49:39 +0330

    completed signUp page

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor(private formBuilder:FormBuilder)
+  {
+
+  }
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group(
+      {
+        email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9]+@[a-z]+\.[a-z]{1,20}')]),
+        username:[null, Validators.required],
+        password : new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9]).{8,}$')])
+      }
+    )
+  }
+
+  signUpForm:FormGroup;
+
+
+}
