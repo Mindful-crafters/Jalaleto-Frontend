a14e3dc586f99299fc44ed804cb868ff1dfa81e0
TheSalimi <amirhosein.srudsari@gmail.com>
2023-11-10 17:11:48 +0330

    add auth service and guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [
-  {path:'',redirectTo:'signup',pathMatch:'full'},
-  {path:'signup',component:SignUpComponent}
+  {path:'dashboard',component:DashboardComponent,canActivate:[AuthGuard]},
+  {path:'signup',component:SignUpComponent},
+  {path:'',redirectTo:'/dashboard',pathMatch:'full'},
 ];
 
 @NgModule({
