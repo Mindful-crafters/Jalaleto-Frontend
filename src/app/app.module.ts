4e809e5dc93eb771a18e29cdc793338ce699c053
TheSalimi <amirhosein.srudsari@gmail.com>
2023-11-10 17:12:43 +0330

    clean app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
 import { SignupModule } from './sign-up/sign-up.module';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +14,8 @@ import { SignupModule } from './sign-up/sign-up.module';
     BrowserModule,
     AppRoutingModule,
     SignupModule,
+    HttpClientModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
