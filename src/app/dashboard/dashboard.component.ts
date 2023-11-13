f83c38b8939333726c8c714e4a1cf0d26ad6e188
TheSalimi <amirhosein.srudsari@gmail.com>
2023-11-13 09:15:06 +0330

    add logout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private auth : AuthService, private router : Router){}
+
+  logOut(){
+    this.auth.logout();
+    this.router.navigate(['login']);
+  }
 }
