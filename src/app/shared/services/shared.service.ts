07009f95cae35d903d063b7db070daaa5c5cfc1e
TheSalimi <amirhosein.srudsari@gmail.com>
2023-11-13 10:32:29 +0330

    add shared service

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -4,13 +4,25 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class Shared {
-    private hashString: string;
+    private hashStringToken: string;
+    private hashStringEmail: string;
+    private resetEmail : string;
 
     constructor() { }
 
-    get getHashString() { return this.hashString; }
+    get getHashString() { return this.hashStringToken; }
+    get getHashStringEmail() { return this.hashStringEmail; }
+    get getEmail() { return this.resetEmail; }
 
     setHashString(token: string) {
-        this.hashString = token;
+        this.hashStringToken = token;
+    }
+
+    setHashStringEmail(hashString: string) {
+        this.hashStringEmail = hashString;
+    }
+
+    setEmail(email: string) {
+        this.resetEmail = email;
     }
 }
