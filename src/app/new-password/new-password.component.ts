6f577e0b746aaf4d09e5a18d915609106ae0a608
TheSalimi <amirhosein.srudsari@gmail.com>
2023-11-13 10:33:11 +0330

    conected to rest pass api

--- src/app/new-password/new-password.component.ts
+++ src/app/new-password/new-password.component.ts
@@ -1,6 +1,7 @@
 import { RestService } from './../shared/services/Rest.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Shared } from '../shared/services/shared.service';
 
 @Component({
   selector: 'app-new-password',
@@ -8,8 +9,9 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./new-password.component.scss']
 })
 export class NewPasswordComponent {
+  NewPasswoedForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private restService: RestService) {
+  constructor(private formBuilder: FormBuilder, private restService: RestService, private shared : Shared) {
 
   }
   emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,12 +28,9 @@ export class NewPasswordComponent {
     const email = this.NewPasswoedForm.getRawValue();
     this.restService.postData<any>('User/SendRestPasswordEmail', email).subscribe(
       (response) => {
-        console.log(response);
+        this.shared.setEmail(this.NewPasswoedForm.get('email').value);
+        this.shared.setHashStringEmail(response['hashString']);
       }
     )
   }
-
-  NewPasswoedForm: FormGroup;
-
-
 }
