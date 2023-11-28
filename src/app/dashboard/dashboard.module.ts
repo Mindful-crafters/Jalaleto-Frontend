d02568ef51726777428156f8f2360c0d53e5845d
TheSalimi <amirhosein.srudsari@gmail.com>
2023-11-28 23:31:48 +0330

    fix change week

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -19,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
         BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule,
+        MatButtonModule
     ]
 })
 export class DashboardModule { }
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -8,7 +8,24 @@ import { Subject, debounceTime } from 'rxjs';
 })
 
 export class TimelineComponent implements OnInit {
-  timelineItems : TimeLineItem[] = [];
+  timelineItems: TimeLineItem[] = [];
+  displayedTimeLine: TimeLineItem[] = [];
+  isContentVisible = false;
+  currentWeek = [1, 2, 3, 4, 5, 6, 7];
+  hoveredBox: number | null = null;
+  private hoverSubject = new Subject<number>();
+
+  historicalEvents: HistoricalEvent[] = [
+    { dayNum: 1, event: 'آذر جشن' },
+    { dayNum: 2, event: 'مبارزه غلیه زنان' },
+    { dayNum: 3, event: 'نیروی دریایی' },
+    { dayNum: 4, event: 'مجلس' },
+    { dayNum: 5, event: 'روز دانشجو' },
+    { dayNum: 10, event: 'روز حسابدار' },
+    { dayNum: 15, event: 'روز پژوهش' },
+    { dayNum: 25, event: 'روز ایدز' },
+    { dayNum: 29, event: 'روز بیمه' },
+  ];
 
   constructor() {
     this.hoverSubject.pipe(debounceTime(200)).subscribe(index => {
@@ -16,21 +33,62 @@ export class TimelineComponent implements OnInit {
     });
   }
 
-  ngOnInit() { 
+  updateTimeLine(direction: string) {
+    const updatedWeek: TimeLineItem[] = [];
+
+    if (direction == 'up') {
+      for (const item of this.displayedTimeLine) {
+        const pastDate = new Date(item.date);
+        pastDate.setDate(item.date.getDate() - 7);
+
+        const WeekItem: TimeLineItem = {
+          date: pastDate,
+          dayName: item.dayName,
+          dayNum: pastDate.getDate(),
+        };
+
+        updatedWeek.push(WeekItem)
+      }
+    }
+    else {
+      for (const item of this.displayedTimeLine) {
+        const pastDate = new Date(item.date);
+        pastDate.setDate(item.date.getDate() + 7);
+
+        const WeekItem: TimeLineItem = {
+          date: pastDate,
+          dayName: item.dayName,
+          dayNum: pastDate.getDate(),
+        };
+
+        updatedWeek.push(WeekItem)
+      }
+    }
+
+    this.displayedTimeLine = updatedWeek;
+    console.log(this.displayedTimeLine)
+  }
+
+  ngOnInit() {
     this.generateTimeline();
+    this.displayedTimeLine = this.timelineItems;
+  }
+
+  isOpen() {
+
   }
 
   generateTimeline() {
     const today = new Date();
 
-    for (let i = 6; i>=0; i--) {
+    for (let i = 0; i < 7; i++) {
       const date = new Date(today);
       date.setDate(today.getDate() + i);
 
       const dayNum = date.getDate(); // Extract the day of the month
       const dayName = this.getDayName(date.getDay());
 
-      this.timelineItems.push({ date, dayName , dayNum });
+      this.timelineItems.push({ date, dayName, dayNum });
     }
   }
 
@@ -38,11 +96,6 @@ export class TimelineComponent implements OnInit {
     const daysOfWeek = ['یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه', 'شنبه'];
     return daysOfWeek[dayIndex];
   }
-
-  boxes = [1, 2, 3, 4, 5, 6, 7];
-  hoveredBox: number | null = null;
-  private hoverSubject = new Subject<number>();
-
 
   onBoxHover(index: number): void {
     this.hoverSubject.next(index);
@@ -53,8 +106,13 @@ export class TimelineComponent implements OnInit {
   }
 }
 
-export interface TimeLineItem{
-  date : Date;
-  dayName : string;
-  dayNum : number;
+export interface TimeLineItem {
+  date: Date;
+  dayName: string;
+  dayNum: number;
+}
+
+export interface HistoricalEvent {
+  dayNum: number;
+  event: string;
 }
