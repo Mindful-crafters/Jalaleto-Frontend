c7cce5ee39663991af7e8e2cabf1c3776ce399c1
azin hashemabadi <azin_hashemabadi@comp.iust.ac.ir>
2023-11-30 23:09:21 +0330

    [add] : homepage

---
+++ src/app/homepage/homepage.module.ts
@@ -0,0 +1,30 @@
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { ProfileComponent } from './profile.component';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '../app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+// import { NavbarComponent } from '../navbar/navbar.component';
+// import { FooterComponent } from '../footer/footer.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
+import { HomepageComponent } from './homepage.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomepageComponent,
+
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomepageModule { }
