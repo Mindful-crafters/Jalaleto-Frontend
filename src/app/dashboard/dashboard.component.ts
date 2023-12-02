438aeed680820cf8f9ba8db5f949189a56072616
azin hashemabadi <azin_hashemabadi@comp.iust.ac.ir>
2023-12-02 15:26:07 +0330

    update doshboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from './../shared/services/auth.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RestService } from '../shared/services/Rest.service';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,42 @@ import { RestService } from '../shared/services/Rest.service';
 })
 export class DashboardComponent {
   isLoggedIn = false;
+
+  private dayArray = ['یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشبه', 'پنجشنبه', 'جمعه', 'شنبه'];
+
+  private date = new Date();
+  public hour: any;
+  public minute: string;
+  public second: string;
+  public ampm: string;
+  public day: string;
+  public month: string;
+  public jalaliMonth;
+  public jalaliYear;
+  public jalaliDay;
+  ngOnInit() {
+    const token = '';
+    setInterval(() => {
+      const date = new Date();
+      this.updateDate(date);
+    }, 1000)
+    this.day = this.dayArray[this.date.getDay()];
+    this.jalaliMonth = moment().locale('fa').format('jMMMM');
+    const jalali = moment().locale('fa');
+    this.jalaliYear = jalali.jYear();
+    this.jalaliDay = jalali.date();
+  }
+  private updateDate(date: Date) {
+    const hours = date.getHours();
+    this.ampm = hours >= 12 ? 'PM' : 'AM';
+    this.hour = hours % 12;
+    this.hour = this.hour ? this.hour : 12;
+    this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+    const minutes = date.getMinutes();
+    this.minute = minutes < 10 ? '0' + minutes : minutes.toString();
+    const seconds = date.getSeconds();
+    this.second = seconds < 10 ? '0' + seconds : seconds.toString();
+  }
 
   constructor(
     private restService: RestService,
