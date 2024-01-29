8db3601dbf80e40b1d4dc90de483c947a7921fc2
azin hashemabadi <azin_hashemabadi@comp.iust.ac.ir>
2024-01-29 19:39:48 +0330

    fix some error

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -9,6 +9,8 @@ import { ShowGroupsComponent } from '../show-groups/show-groups.component';
 import { Component, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import * as moment from 'jalali-moment';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
@@ -22,6 +24,7 @@ export class EditProfileComponent {
     private formBuilder: FormBuilder, private restService: RestService,
     private toastr: ToastrService,
     private datePipe: DatePipe,
+    private sanitizer: DomSanitizer,
     @Inject(MAT_DIALOG_DATA) public fruits: any) {
     console.log(fruits)
   }
@@ -59,7 +62,8 @@ export class EditProfileComponent {
   @ViewChild('fileInput') fileInput: any;
   selectedImage: string | null = null;
   selectedFile: File | null = null;
-  imageLink: string | null = null;
+  old: File | null = null;
+  imageLink: SafeResourceUrl | null = null;
 
   emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   validateAge(control: AbstractControl): ValidationErrors | null {
@@ -72,12 +76,24 @@ export class EditProfileComponent {
       return { customError: true };
     }
   }
-
   onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0] as File;
-    this.data.imagePath = URL.createObjectURL(this.selectedFile);
+    console.log('aaa',event.target.files)
+      this.selectedFile = event.target.files[0] as File;
+      this.data.imagePath = URL.createObjectURL(this.selectedFile);
   }
+  
 
+  // onFileSelected(event: any) {
+  //   this.selectedFile = event.target.files[0] as File;
+  //   this.data.imagePath = URL.createObjectURL(this.selectedFile);
+  // }
+  // onFileSelected(event: any) {
+  //   if (event.target.files.length > 0) {
+  //     // اگر یک فایل انتخاب شده باشد
+  //     this.selectedFile = event.target.files[0] as File;
+  //     this.data.imagePath = URL.createObjectURL(this.selectedFile);
+  //   }
+  // }
   ngOnInit() {
     this.restService.post("User/ProfileInfo", null).subscribe((res: ProfileResult) => {
 
@@ -87,7 +103,9 @@ export class EditProfileComponent {
       this.data.Email = res.email;
       this.data.Birthday = res.birthday;
       this.data.imagePath = res.imagePath;
-      this.data.Password = res.password;
+      this.imageLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.data.imagePath) as SafeResourceUrl;
+      
+      console.log('img',this.data.imagePath);
       this.data.interests = res.interests;
       const parts = res.birthday.split('/');
       if (parts.length === 3) {
@@ -98,21 +116,41 @@ export class EditProfileComponent {
         const format = this.datePipe.transform(new Date(year, month, day), 'yyyy-MM-dd');
         this.data.Birthday = format;
       }
+      
+
     });
   }
   submit() {
-    console.log(this.selectedFile);
+    this.data.Birthday = this.datePipe.transform(this.data.Birthday, 'yyyy-MM-dd').toString();
+    console.log('iiii',this.data.imagePath);
+    console.log('sss',this.imageLink);
+    console.log('image',this.selectedFile);
+    
+    if (this.data.Password === '' || this.data.Password === null) {
+      this.data.Password = null;
+      console.log('pppp',this.data.Password);
+    }
     const formData = new FormData;
     formData.append('FirstName', this.data.FirstName);
     formData.append('LastName', this.data.LastName);
     formData.append('UserName', this.data.UserName);
     formData.append('BirthDay', this.data.Birthday);
-    formData.append('Image', this.selectedFile);
+    
+    if(this.selectedFile!=null){
+      formData.append('Image', this.selectedFile);
+    }
+    else
+    {
+      const imageUrl: string = this.imageLink.toString();
+      formData.append('Image',imageUrl);
+      console.log('goz',imageUrl);
+    }
     formData.append('Password', this.data.Password);
+    console.log(formData)
     this.fruits.forEach((interest, index) => {
       formData.append(`interests[${index}]`, interest);
     });
-
+    
     this.restService.post<any>('User/EditProfile', formData).subscribe(
       (response) => {
         if (response['success']) {
@@ -129,7 +167,20 @@ export class EditProfileComponent {
       },
     )
   }
+  
+// Convert data URI to Blob
+dataURItoBlob(dataURI: string): Blob {
+  const byteString = atob(dataURI.split(',')[1]);
+  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
 
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ab], { type: mimeString });
+}
   updateData() {
     this.requestData = {
       FirstName: "",
@@ -164,3 +215,4 @@ interface ProfileResult {
   password: string,
   interests : string[]
 }
+
